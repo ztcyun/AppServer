97ff81a4719cdee278785289128b892face3b783
Sergey Kirichenko <sk81.biz@gmail.com>
2020-07-31 12:00:23 +0300

    add mysql script

---
+++ sql/alter_priv.sql
@@ -0,0 +1,4 @@
+ALTER USER "onlyoffice_user"@"localhost" IDENTIFIED WITH mysql_native_password BY "onlyoffice_pass";
+GRANT ALL PRIVILEGES ON *.* TO "onlyoffice_user"@"localhost";
+UPDATE core_user SET email = 'paul.bannov@gmail.com';
+UPDATE core_usersecurity SET pwdhash = 'vLFfghR5tNV3K9DKhmwArV+SbjWAcgZZzIDTnJ0JgCo=', pwdhashsha512 = 'USubvPlB+ogq0Q1trcSupg==';
