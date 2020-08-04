5a0fbb14b297022da7850328d946b804c57f4433
Sergey Kirichenko <sk81.biz@gmail.com>
2020-08-04 16:13:05 +0300

    fix error mysql connect

--- sql/alter_priv.sql
+++ sql/alter_priv.sql
@@ -1,4 +1,6 @@
-ALTER USER 'onlyoffice_user' IDENTIFIED WITH mysql_native_password BY 'onlyoffice_pass';
-GRANT ALL PRIVILEGES ON *.* TO 'onlyoffice_user';
+DROP USER 'onlyoffice_user'@'localhost';
+CREATE USER 'onlyoffice_user'@'localhost';
+ALTER USER 'onlyoffice_user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'onlyoffice_pass';
+GRANT ALL PRIVILEGES ON *.* TO 'onlyoffice_user'@'localhost';
 UPDATE core_user SET email = 'paul.bannov@gmail.com';
 UPDATE core_usersecurity SET pwdhash = 'vLFfghR5tNV3K9DKhmwArV+SbjWAcgZZzIDTnJ0JgCo=', pwdhashsha512 = 'USubvPlB+ogq0Q1trcSupg==';
