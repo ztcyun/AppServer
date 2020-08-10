e3b6b2431e6061005042d48bcc7280c8dbf4b4cd
Sergey Kirichenko <sk81.biz@gmail.com>
2020-08-10 12:49:49 +0300

    Add script environment

---
+++ build/install/docker/config/api/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sed -i "s|Server=.*;Pooling=|Server=$MYSQL_HOST;Port=3306;Database=$MYSQL_DATABASE;User ID=$MYSQL_USER;Password=$MYSQL_PASSWORD;Pooling=|g" /app/onlyoffice/config/appsettings.test.json
+sed -i "s|localhost|$KAFKA_HOST|g" /app/onlyoffice/config/kafka.test.json
+
+dotnet $DOTNET_RUN --urls=http://0.0.0.0:5050 --pathToConf=/app/onlyoffice/config/ --ENVIRONMENT=$DOTNET_ENV --$STORAGE_ROOT=/app/onlyoffice/data/ --log:dir=/var/log/onlyoffice --log:name=$DOTNET_LOG_NAME
