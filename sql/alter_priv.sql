561e4a54b78bfe66d4c93da9e98512bdb7b91e0b
Sergey Kirichenko <sk81.biz@gmail.com>
2020-09-02 09:50:19 +0300

    fix ASC.Files

--- sql/alter_priv.sql
+++ sql/alter_priv.sql
@@ -1,2 +1,2 @@
-UPDATE core_user SET email = 'paul.bannov@gmail.com';
+/* UPDATE   SET email = 'paul.bannov@gmail.com';  */
 UPDATE core_usersecurity SET pwdhash = 'vLFfghR5tNV3K9DKhmwArV+SbjWAcgZZzIDTnJ0JgCo=', pwdhashsha512 = 'USubvPlB+ogq0Q1trcSupg==';
