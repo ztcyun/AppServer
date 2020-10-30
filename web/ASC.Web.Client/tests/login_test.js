9b8edd987efa6f1e4f169dff42db7689f891e71f
AlexeySafronov <alexey.safronov@onlyoffice.com>
2020-10-30 18:17:03 +0300

    Web: Client: Added test for login success

--- web/ASC.Web.Client/tests/login_test.js
+++ web/ASC.Web.Client/tests/login_test.js
@@ -1,5 +1,7 @@
 const browser = process.env.profile || "chromium";
 const deviceType = process.env.DEVICE_TYPE || "desktop";
+const login = process.env.login || "user4test@example.com";
+const password = process.env.password || "11111111";
 
 Feature(`Login Page on '${browser}' with '${deviceType}' dimension`);
 
@@ -9,7 +11,7 @@ Scenario("Compare Login Page @visual-test", async ({ I }) => {
   I.saveScreenshot(`login.${browser}.${deviceType}.png`);
   I.seeVisualDiff(`login.${browser}.${deviceType}.png`, {
     tolerance: 2,
-    prepareBaseImage: true,
+    prepareBaseImage: false,
   });
 });
 
@@ -19,4 +21,12 @@ Scenario("Test login error", ({ I }) => {
   I.fillField("password", secret("0000000"));
   I.click("Sign In");
   I.see("user not found");
+});
+
+Scenario("Test login success", ({ I }) => {
+  I.amOnPage("/login");
+  I.fillField("login", login);
+  I.fillField("password", secret(password));
+  I.click("Sign In");
+  I.amOnPage("/");
 });
